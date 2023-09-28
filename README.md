5eb1b1cb3121abf9dbec1f335d69d8077bb74155
Luis Salas <lhozdroid@gmail.com>
2023-09-27 23:15:28 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# modal
+#Modal
+
 A simple JavaScript class for creating modal dialogs.
+
+##Usage
+
+###Creating a Success Modal
+
+```javascript
+const successModal = Modal.success("This is a success message");
+successModal.close(); // Close the modal
+````
+
+###Creating a Warning Modal
+
+```javascript
+const warningModal = Modal.warning("This is a warning message");
+warningModal.close(); // Close the modal
+```
+
+###Creating a Danger Modal
+
+```javascript
+const dangerModal = Modal.danger("This is a danger message");
+dangerModal.close(); // Close the modal
+```
+
+###Creating a Light Modal
+
+```javascript
+const lightModal = Modal.light("This is a light message");
+lightModal.close(); // Close the modal
+```
+
+###Creating a Dark Modal
+
+```javascript
+const darkModal = Modal.dark("This is a dark message");
+darkModal.close(); // Close the modal
+```
+
+###Creating a Loading Modal
+
+```javascript
+const loadingModal = Modal.loading("Loading...", ModalSpinner.SPINNER);
+// Do some asynchronous operation
+// loadingModal.close(); // Close the modal when the operation is done
+```
+
+##API
+
+
+###Modal Options
+
+- **type**: Type of the modal (ModalType.DANGER, ModalType.WARNING, ModalType.SUCCESS, ModalType.DARK, ModalType.LIGHT)
+- **class**: Additional CSS class for the modal
+- **title**: Title of the modal
+- **message**: Content/message of the modal
+- **closable**: Boolean, whether the modal is closable or not
+- **spinner**: Type of spinner for loading modals (ModalSpinner.SPINNER, ModalSpinner.CIRCLE, etc.)
+
+###Methods
+
+- **close()**: Closes the modal
+
+###Modal Types
+
+- ModalType.DANGER
+- ModalType.WARNING
+- ModalType.SUCCESS
+- ModalType.DARK
+- ModalType.LIGHT
+
+###Spinner Types
+
+- ModalSpinner.CIRCLE
+- ModalSpinner.GEAR
+- ModalSpinner.GEARS
+- ModalSpinner.HOURGLASS
+- ModalSpinner.IPHONE_1
+- ModalSpinner.IPHONE_2
+- ModalSpinner.SEARCH
+- ModalSpinner.SPINNER
---
+++ src/js/modal.js
@@ -0,0 +1,264 @@
+/**
+ * Creates a dialog or loading modal for vanilla JS
+ */
+class Modal {
+    #config = {
+        type: ModalType.DANGER,
+        class: null,
+        title: "Alert",
+        message: "Modal message",
+        closable: true,
+        spinner: null
+    };
+
+    #backdrop = null;
+    #modal = null;
+
+    /**
+     * Default constructor
+     * @param config
+     */
+    constructor(config) {
+        this.#config = this.#extend(this.#config, config);
+
+        this.#setBackdrop();
+        this.#setModal();
+    }
+
+    /**
+     * Creates a success modal
+     * @param message
+     * @param title
+     * @param closable
+     */
+    static success(message, title = "Success", closable = true) {
+        return new Modal({
+            "type": ModalType.SUCCESS,
+            "title": title,
+            "message": message,
+            "closable": closable
+        });
+    }
+
+    /**
+     * Creates a warning modal
+     * @param message
+     * @param title
+     * @param closable
+     */
+    static warning(message, title = "Warning", closable = true) {
+        return new Modal({
+            "type": ModalType.WARNING,
+            "title": title,
+            "message": message,
+            "closable": closable
+        });
+    }
+
+    /**
+     * Creates a danger modal
+     * @param message
+     * @param title
+     * @param closable
+     */
+    static danger(message, title = "Danger", closable = true) {
+        return new Modal({
+            "type": ModalType.DANGER,
+            "title": title,
+            "message": message,
+            "closable": closable
+        });
+    }
+
+    /**
+     * Creates a light modal
+     * @param message
+     * @param title
+     * @param closable
+     */
+    static light(message, title = "Message", closable = true) {
+        return new Modal({
+            "type": ModalType.LIGHT,
+            "title": title,
+            "message": message,
+            "closable": closable
+        });
+    }
+
+    /**
+     * Creates a dark modal
+     * @param message
+     * @param title
+     * @param closable
+     */
+    static dark(message, title = "Message", closable = true) {
+        return new Modal({
+            "type": ModalType.DARK,
+            "title": title,
+            "message": message,
+            "closable": closable
+        });
+    }
+
+    /**
+     * Creates a loading modal
+     * @param message
+     * @param spinner
+     */
+    static loading(message, spinner = ModalSpinner.SPINNER) {
+        return new Modal({
+            "message": message,
+            "spinner": spinner
+        });
+    }
+
+    /**
+     * Closes the modal
+     */
+    close() {
+        this.#backdrop.classList.remove("fade-in-fwd");
+        this.#modal.classList.remove("slide-in-blurred-top");
+
+        this.#backdrop.classList.add("fade-out");
+        this.#modal.classList.add("slide-out-blurred-top");
+
+        setTimeout(() => {
+            this.#backdrop.remove();
+            this.#modal.remove();
+        }, 1000);
+    }
+
+    /**
+     *
+     */
+    #setBackdrop() {
+        // language=HTML
+        this.#backdrop = new DOMParser().parseFromString(`
+			<div class="modal-backdrop fade-in-fwd"></div>
+        `, "text/html").body.firstChild;
+
+        document.querySelector("body").append(this.#backdrop);
+    }
+
+    /**
+     *
+     */
+    #setModal() {
+        const header = this.#header();
+        const body = this.#body();
+
+        // language=HTML
+        this.#modal = new DOMParser().parseFromString(`
+            <div class="modal slide-in-blurred-top ${this.#config.type} ${this.#config.class != null ? this.#config.class : ""}">
+                ${header}
+                ${body}
+            </div>
+        `, "text/html").body.firstChild;
+
+        // Sets the close action (if present)
+        if (this.#config.spinner == null && this.#config.closable) {
+            this.#modal.querySelector("div.close").addEventListener("click", (e) => {
+                this.close();
+            });
+        }
+
+        // Adds the modal to the body
+        document.querySelector("body").append(this.#modal);
+    }
+
+    /**
+     *
+     */
+    #header() {
+        // language=HTML
+        return this.#config.spinner == null ? `
+            <div class="modal-header">
+                <h4>${this.#config.title}</h4>
+                ${this.#config.closable ? `<div class="close"></div>` : ""}
+            </div>
+        ` : "";
+    }
+
+    /**
+     *
+     */
+    #body() {
+        if (this.#config.spinner == null) {
+            // language=HTML
+            return `
+                <div class="modal-body">
+                    ${this.#config.message}
+                </div>
+            `;
+        } else {
+            // language=HTML
+            return `
+                <div class="modal-spinner">
+                    <div class="spinner-icon ${this.#config.spinner}"></div>
+                    <span class="spinner-message">${this.#config.message}</span>
+                </div>
+            `;
+        }
+    }
+
+    /**
+     *
+     * @param base
+     * @param extensions
+     * @returns {{}|*}
+     */
+    #extend(base, ...extensions) {
+        if (!base) {
+            return {};
+        }
+
+        for (const obj of extensions) {
+            if (!obj) {
+                continue;
+            }
+
+            for (const [key, value] of Object.entries(obj)) {
+                switch (Object.prototype.toString.call(value)) {
+                    case "[object Object]":
+                        base[key] = base[key] || {};
+                        base[key] = this.#extend(base[key], value);
+                        break;
+                    case "[object Array]":
+                        base[key] = this.#extend(new Array(value.length), value);
+                        break;
+                    default:
+                        base[key] = value;
+                }
+            }
+        }
+
+        return base;
+    }
+}
+
+/**
+ * Modal types
+ * @type {{}}
+ */
+const ModalType = {
+    DANGER: "danger",
+    WARNING: "warning",
+    SUCCESS: "success",
+    DARK: "dark",
+    LIGHT: "light"
+};
+
+/**
+ * Modal spinners
+ * @type {{}}
+ */
+const ModalSpinner = {
+    CIRCLE: "circle",
+    GEAR: "gear",
+    GEARS: "gears",
+    HOURGLASS: "hourglass",
+    IPHONE_1: "iphone-1",
+    IPHONE_2: "iphone-2",
+    SEARCH: "search",
+    SPINNER: "spinner"
+};
